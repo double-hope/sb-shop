ea3dfb9eed2a527dc5aa912094f5b31c52b05cbc
double-hope <nadia.prohorchuk@gmail.com>
2023-10-27 18:03:17 +0300

    add ActionNames to separated class

---
+++ SB.Shared/Models/ActionNames.cs
@@ -0,0 +1,11 @@
+﻿namespace SB.Shared.Models
+{
+    public class ActionNames
+    {
+        public string SearchDublicatedPhone;
+        public string SendExcelFlow;
+        public string SendBirthdayEmail;
+        public string RetrieveLogicalNames;
+        public string RetrieveViewNames;
+    }
+}
